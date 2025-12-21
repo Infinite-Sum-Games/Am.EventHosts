5d4fb20a87b9d92528f254f760d365b4c4e5e774
vijaysb0613 <vijaysb2006@gmail.com>
2025-12-21 15:42:43 +0530

    fix:update api endpoints

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,8 +1,8 @@
-// For now, let's use a relative URL to leverage the Vite proxy.
 const CURRENT_BASE_URL: string = "/api/v1";
 
 export const api = {
-  LOGIN: `${CURRENT_BASE_URL}/auth/admin/login`,
-  LOGOUT: `${CURRENT_BASE_URL}/auth/admin/logout`,
-  SESSION: `${CURRENT_BASE_URL}/auth/admin/session`,
+  LOGIN: `${CURRENT_BASE_URL}/auth/organizer/login`,
+  DASHBOARD: `${CURRENT_BASE_URL}/organizers/dashboard`,
+  PARTICIPANTS: (eventId: string) =>
+    `${CURRENT_BASE_URL}/organizers/dashboard/${eventId}`,
 };
