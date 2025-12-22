f92ac198cb376acd4d129da5fc697ef01026adb0
SuperSecureHuman <supersecurehuman@gmail.com>
2025-12-22 23:33:43 +0530

    Api commit

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,8 +1,8 @@
 export const api = {
-  LOGIN: "http://localhost:8080/api/v1/auth/organizer/login",
-  DASHBOARD: "http://localhost:8080/api/v1/organizers/dashboard",
+  LOGIN: "http://localhost:9000/api/v1/auth/organizer/login",
+  DASHBOARD: "http://localhost:9000/api/v1/organizers/dashboard",
   PARTICIPANTS: (eventId: string) =>
-    `http://localhost:8080/api/v1/organizers/dashboard/${eventId}`,
-  SESSION: "http://localhost:8080/api/v1/auth/organizer/session",
-  LOGOUT: "http://localhost:8080/api/v1/auth/organizer/logout",
+    `http://localhost:9000/api/v1/organizers/dashboard/${eventId}`,
+  SESSION: "http://localhost:9000/api/v1/auth/organizer/session",
+  LOGOUT: "http://localhost:9000/api/v1/auth/organizer/logout",
 };
