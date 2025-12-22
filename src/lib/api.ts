919f1f013a54daf577f9edcb0f385eacb05909f9
vijaysb0613 <vijaysb2006@gmail.com>
2025-12-22 14:33:50 +0530

    fix:update session url

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -3,6 +3,6 @@ export const api = {
   DASHBOARD: "http://localhost:8080/api/v1/organizers/dashboard",
   PARTICIPANTS: (eventId: string) =>
     `http://localhost:8080/api/v1/organizers/dashboard/${eventId}`,
-  SESSION: "http://localhost:8080/api/v1/auth/user/session",
+  SESSION: "http://localhost:8080/api/v1/auth/organizer/session",
   LOGOUT: "http://localhost:8080/api/v1/auth/user/logout",
 };
