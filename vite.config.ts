80751e82353b802521c7c6cb05cfaf8d7bef85b7
vijaysb0613 <vijaysb2006@gmail.com>
2025-12-25 00:48:30 +0530

    restore vite config

--- vite.config.ts
+++ vite.config.ts
@@ -26,7 +26,7 @@ export default defineConfig({
 		port: 4173,
 		proxy: {
 			"/api/v1": {
-				target: "http://localhost:8080",
+				target: "http://localhost:9000",
 				changeOrigin: true,
 			},
 		},
