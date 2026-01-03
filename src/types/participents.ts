ba0cfee49f55a693e5e6effd77aed29fd911c862
KiranRajeev-KV <kiranrajeevkv@gmail.com>
2026-01-03 10:54:30 +0530

    feat: add Participant interface for event registration

---
+++ src/types/participents.ts
@@ -0,0 +1,9 @@
+export interface Participant {
+  team_name?: string;
+  student_name: string;
+  college: string;
+  city: string;
+  email: string;
+  phone_number: string;
+  is_amrita_student: boolean;
+}
